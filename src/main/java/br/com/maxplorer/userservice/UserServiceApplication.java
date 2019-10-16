f5f17b1de58047dff3e9a3a3e466872a4ac9e433
Roberto Amadeu Neto <roberto.neto@beblue.com.br>
2019-10-15 21:53:27 -0300

    Add support to Consul

--- src/main/java/br/com/maxplorer/userservice/UserServiceApplication.java
+++ src/main/java/br/com/maxplorer/userservice/UserServiceApplication.java
@@ -2,14 +2,20 @@ package br.com.maxplorer.userservice;
 
 import br.com.maxplorer.userservice.adapter.messaging.MessagingChannels;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 @EnableBinding(value = MessagingChannels.class)
+@EnableDiscoveryClient
+@Configuration
+@EnableAutoConfiguration
 public class UserServiceApplication {
 
     public static void main(String[] args) {
